01081105a6b92f66ec2aaa014702d4f7b2021705
Ina <inatsetsova@gmail.com>
2016-03-12 14:01:48 +0000

    functioning tip calculator

--- lib/tip_calculator/tip_calculator.rb
+++ lib/tip_calculator/tip_calculator.rb
@@ -1,11 +1,25 @@
 class TipCalculator
-  def initialize(bill_amount)
-    @bill_amount = bill_amount
+  def initialize(bill_amount, tip_percent = 15)
+    @bill_amount = Float(bill_amount)
+    @tip_percent = Float(tip_percent)/100
   end
+
   def tip
-    0
+    format_price(tip_amount)
   end
+
   def bill
-    0
+    format_price(@bill_amount + tip_amount)
   end
+
+  private
+
+  def tip_amount
+    @bill_amount * @tip_percent
+  end
+
+  def format_price(amount)
+     sprintf("%.2f",(amount))
+  end
+
 end
--- spec/tip_calculator/tip_calculator_spec.rb
+++ spec/tip_calculator/tip_calculator_spec.rb
@@ -2,10 +2,31 @@ require "spec_helper"
 require "tip_calculator/tip_calculator"
 
 describe TipCalculator do
-  it "returns a $0 tip for a $0 bill" do
-    expect(described_class.new("0").tip).to eq(0)
+  it "returns a correct tip for $100 bill" do
+    expect(described_class.new("100").tip).to eql("15.00")
   end
-  it "returns a $0 total bill for a $0 bill amount" do
-    expect(described_class.new("0").bill).to eq(0)
+
+  it "returns a correct total bill for $100 bill" do
+    expect(described_class.new("100").bill).to eql("115.00")
+  end
+
+  it "returns a correct tip for $10 bill" do
+    expect(described_class.new("10").tip).to eql("1.50")
+  end
+
+  it "returns a correct bill for $10 bill" do
+    expect(described_class.new("10").bill).to eql("11.50")
+  end
+
+  it "returns a correct bill for $100 bill with 20% tip" do
+    expect(described_class.new("100", "20").tip).to eql("20.00")
+  end
+
+  it "returns a correct bill for $100 bill with 20% tip" do
+    expect(described_class.new("100", "20").bill).to eql("120.00")
+  end
+
+  it "round up the tip amount for fractions" do
+    expect(described_class.new("11.25").tip).to eql("1.69")
   end
 end
