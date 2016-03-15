d93b8c5177fee08e2cacd96649da378c3d5f97d5
Ina <inatsetsova@gmail.com>
2016-03-15 21:11:06 +0000

    changes naming

--- lib/string_length/string_length.rb
+++ lib/string_length/string_length.rb
@@ -2,16 +2,16 @@ require "stringio"
 
 class StringHelper
 
-  def initialize(input=$stdin,io=$stdout)
-    @io = io
+  def initialize(input=$stdin,output=$stdout)
+    @output = output
     @input = input
   end
 
   def string_length
-    @io.puts "Hi, what's your string?"
+    @output.puts "Hi, what's your string?"
 
     string = @input.gets.chomp
 
-    @io.puts "#{string} has #{string.length} characters."
+    @output.puts "#{string} has #{string.length} characters."
   end
 end
